27fb2d9d92bf09a36ff7656639620790a0071d2b
Vladlin Moiseenko <mvladlin@gmail.com>
2019-02-28 21:56:12 +0300

    update

--- ios-itodolist/View/MainViewController.swift
+++ ios-itodolist/View/MainViewController.swift
@@ -13,7 +13,7 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupNavigationBarItems()
+        //setupNavigationBarItems()
         configureTableView()
         bindRx()
     }
