35c3bc5e545b673ce3261053b051be56b114cea7
Vladlin Moiseenko <mvladlin@gmail.com>
2019-03-05 09:15:42 +0300

    update

--- ios-itodolist/View/AddViewController.swift
+++ ios-itodolist/View/AddViewController.swift
@@ -1,35 +1,50 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import Action
 
 class AddViewController: UIViewController {
     
     @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var addViewModel: AddViewModel!
     var disposeBag = DisposeBag()
     
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         titleTextField.text = "New task"
+        
+        self.navigationItem.rightBarButtonItem?.rx.action = CocoaAction {
+            self.activityIndicator.startAnimating()
+            
+            self.addViewModel = AddViewModel()
+            
+            let title = self.titleTextField.text
+            
+            self.addViewModel?.apiTaskCreate(title!)
+            
+            self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(self.doOnward), userInfo: nil, repeats: false)
+
+            return Observable.empty().delaySubscription(2, scheduler: MainScheduler.instance) 
+        }
+          
     }
 
+    @objc func doOnward() {
+        
+        guard let vc = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() else {
+            return
+        }
+        self.navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    @IBAction func addAction(_ sender: Any) {
-
-        addViewModel = AddViewModel()
-        
-        let title = self.titleTextField.text
-        
-        self.addViewModel?.apiTaskCreate(title!)
-        
-        //DataSubject.AddedNotification.on(.next(addAction(<#T##sender: Any##Any#>)),
-        self.navigationController?.popViewController(animated: true)
-//        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func closeAction(_ sender: Any) {
@@ -37,7 +52,6 @@ class AddViewController: UIViewController {
 //        dismiss(animated: true, completion: nil)
     }
     
-
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
     }
--- ios-itodolist/ViewModel/AddViewModel.swift
+++ ios-itodolist/ViewModel/AddViewModel.swift
@@ -10,15 +10,10 @@ class AddViewModel {
         
         apiController = ApiController()
         
-        //let title: String = "new title"
-        
         let jsonParam = try? JSONEncoder().encode(TaskDataSave(title: title))
         let param = try? JSONSerialization.jsonObject(with: jsonParam!, options: []) as? [String : Any]
         
-        //print("param", param)
-        
         apiController?.taskCreate(param: param as! [String : Any], success: {modelStatus in
-                print(modelStatus)
 //            if modelTask.status == 1 {
 //
 //            }
